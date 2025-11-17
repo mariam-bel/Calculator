749faeaf23d3eac33af8519ed68b20f00cd13029
mariam-bel <merimarabel@gmail.com>
2025-11-17 10:59:07 +0100

    Calculadora

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.calculator;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView pantalla;
+    TextView numero1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        pantalla = findViewById(R.id.pantalla);
+        numero1 = findViewById(R.id.button5);
+
+        pantalla.setText(String.valueOf(numero1));
+
+
+
+    }
+
+
+}
